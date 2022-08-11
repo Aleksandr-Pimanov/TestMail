5bc3f6d260ad3cbae89d7791752e964121dd9b07
Aleksandr Pimanov <pimanov.aleksandr00@mail.ru>
2022-08-11 18:27:58 +0400

    Initial Commit

---
+++ TestMail/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestMail
+//
+//  Created by новый пользователь on 11.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
